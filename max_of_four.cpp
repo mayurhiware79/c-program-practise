46f966a67cc93cd6f86dd09a129ee4ddbbffb833
Mayur Hiware <157942713+mayurhiware79@users.noreply.github.com>
2025-02-07 16:37:56 +0530

    Update max_of_four.cpp

--- max_of_four.cpp
+++ max_of_four.cpp
@@ -24,3 +24,34 @@ int main() {
     
     return 0;
 }
+
+/*
+#include <stdio.h>
+/*
+Add `int max_of_four(int a, int b, int c, int d)` here.
+*/
+int max_of_four(int a, int b, int c, int d){
+    int max = a;
+    int arr[4];
+    arr[0] = a;
+    arr[1]= b;
+    arr[2] = c;
+    arr[3] = d;
+    
+    for(int i = 0;i<4;i++){
+        if(max < arr[i]){
+            max = arr[i];
+        }
+    }
+    return max;
+}
+
+int main() {
+    int a, b, c, d;
+    scanf("%d %d %d %d", &a, &b, &c, &d);
+    int ans = max_of_four(a, b, c, d);
+    printf("%d", ans);
+    
+    return 0;
+}
+*/
