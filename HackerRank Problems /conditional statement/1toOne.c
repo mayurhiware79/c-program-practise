89b6c68a2c83d69abcc4eff5581cd10a403951e6
Mayur Hiware <157942713+mayurhiware79@users.noreply.github.com>
2025-02-13 19:02:02 +0530

    Update 1toOne.c

--- HackerRank Problems /conditional statement/1toOne.c
+++ HackerRank Problems /conditional statement/1toOne.c
@@ -1,16 +1,4 @@
-#include <assert.h>
-#include <limits.h>
-#include <math.h>
-#include <stdbool.h>
-#include <stddef.h>
-#include <stdint.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-
-char* readline();
-
-
+//TEST CASE 10 NOT SATISFIED
 
 int main()
 {
@@ -21,61 +9,38 @@ int main()
     if (n_endptr == n_str || *n_endptr != '\0') { exit(EXIT_FAILURE); }
 
     // Write Your Code Here
-    if(n > 0){
-        switch(n){
-        case 1: printf("one");
-            break;
-        case 2: printf("two");
-            break;
-        case 3: printf("three");
-            break;
-        case 4 : printf("four");
-            break;
-        case 5 : printf("five");
-            break;
-        case 6 : printf("six");
-            break;
-        case 7 : printf("seven");
-            break;
-        case 8 : printf("eight");
-            break;
-         default: printf("Greater than 9");                       
-    } 
-    }else {
-        printf("pls enter positive int");
+    // if(n >= 1 && n <=9 ){
+    //     switch(n){
+    //     case 1: printf("one");
+    //         break;
+    //     case 2: printf("two");
+    //         break;
+    //     case 3: printf("three");
+    //         break;
+    //     case 4 : printf("four");
+    //         break;
+    //     case 5 : printf("five");
+    //         break;
+    //     case 6 : printf("six");
+    //         break;
+    //     case 7 : printf("seven");
+    //         break;
+    //     case 8 : printf("eight");
+    //         break;     
+    // } 
+    // }else {
+    //     printf("Greater than 9");                       
         
+    // }
+
+
+    //SUCCESS
+     char *number[30] = {"one","two","three","four","five","six","seven","eight","nine"};
+    
+    if(n >= 1&& n <=9){
+        printf("%s",number[n-1]);
+    }else {
+        printf("Greater than 9");
     }
     return 0;
-}
-
-char* readline() {
-    size_t alloc_length = 1024;
-    size_t data_length = 0;
-    char* data = malloc(alloc_length);
-
-    while (true) {
-        char* cursor = data + data_length;
-        char* line = fgets(cursor, alloc_length - data_length, stdin);
-
-        if (!line) { break; }
-
-        data_length += strlen(cursor);
-
-        if (data_length < alloc_length - 1 || data[data_length - 1] == '\n') { break; }
-
-        size_t new_length = alloc_length << 1;
-        data = realloc(data, new_length);
-
-        if (!data) { break; }
-
-        alloc_length = new_length;
-    }
-
-    if (data[data_length - 1] == '\n') {
-        data[data_length - 1] = '\0';
-    }
-
-    data = realloc(data, data_length);
-
-    return data;
 }
